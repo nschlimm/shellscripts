19e321e9fe6421ce96bc5cf0e6b83a61c9709c37
Niklas Schlimm <niklas.schlimm@provinzial.com>
2017-02-18 20:53:31 +0100

    Rubbish deleted

--- s.sh
+++
@@ -1,8 +0,0 @@
-            echo "Projektname?"
-            read projektname
-            error=$(cd $projektname 2>&1 )
-            echo $error
-            echo $error
-            pushd $projektname
-            ls -a
-            popd
